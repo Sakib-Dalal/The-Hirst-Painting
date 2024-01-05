dbb6c2066932a60aba696ff71034219f851d44f9
Sakib_Dalal <sakibdalal73@gmail.com>
2024-01-05 12:26:43 +0530

    Stage 3

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import colorgram
 """ for more info about colorgram visit link:- https://pypi.org/project/colorgram.py/"""
 
+import turtle as t
+import random
+
 color = colorgram.extract("image.jpg", 30)
 
 my_colors_list = []
@@ -10,3 +13,11 @@ for i in range(len(color)):
     my_colors_list.append(my_tuple_rgb)
 
 print(my_colors_list)
+
+t.colormode(255)
+my_turtle = t.Turtle()
+my_screen = t.Screen()
+
+my_turtle.dot(20, random.choice(my_colors_list))
+
+my_screen.exitonclick()
